96906da0aae890aaa45e25af9d234b288d18cf70
xufeng <81959804@qq.com>
2011-12-24 18:24:14 +0800

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,5 @@
+class CommentsController < ApplicationController
+  def create
+    debugger
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3c890ebc2f195ba20c7279f30497763bf0e3f4cbfc9475ff7af261d4332885845445dd73fb1b5d849bc9f80535567ac3f631dd02d656336d73fbc4818e7a7846'
